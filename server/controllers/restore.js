258ff4cc428d69ffe74ca34072bd72240ed414f8
Claire Almozinos <claire@localhost.localdomain>
2016-03-24 02:03:19 -0700

    #HS-13: add comment

--- server/controllers/restore.js
+++ server/controllers/restore.js
@@ -2,6 +2,10 @@
 // Controller Restoration
 //
 module.exports = function initRestoreController(managers, controllers) {
+
+  //
+  //
+  //
   controllers.launchRestore = function(req, res) {
     const userId = req.body.userId; // Be able to change
     const saveId = req.body.saveId; // Be able to change
@@ -17,18 +21,27 @@ module.exports = function initRestoreController(managers, controllers) {
     res.redirect('/restore');
   };
 
+  //
+  // Get restodeId and launch function startRestore in the manager
+  //
   controllers.startRestore = function(req, res) {
     const restoreId = req.body.restoreId;
     managers.startRestore(restoreId);
     res.redirect('/restore');
   };
 
+  //
+  // Get restodeId and launch function restoreFinish in the manager
+  //
   controllers.restoreFinish = function(req, res) {
     const restoreId = req.body.restoreId;
     managers.restoreFinish(restoreId);
     res.redirect('/restore');
   };
 
+  //
+  // Get restodeId and launch function restoreSuccess in the manager
+  //
   controllers.restoreSuccess = function(req, res) {
     const restoreId = req.body.restoreId;
     managers.restoreSuccess(restoreId);
