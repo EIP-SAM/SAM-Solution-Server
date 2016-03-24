387814247fd00ca15480fd71b8aff4ced14d7b75
Claire Almozinos <claire@localhost.localdomain>
2016-03-24 01:22:53 -0700

    #HS-10: create routes for restoration, restore, launch restore and restore_is_start

---
+++ server/routes/restore.js
@@ -0,0 +1,19 @@
+module.exports = function initRestoreRoutes(app, ensureLoggedIn,
+  ensureLoggedOut, controllers) {
+
+  /**
+  ** BASE
+  */
+  app.get('/restore', function(req, res) {
+    res.render('program_save_restore_test.ejs');
+  });
+
+  app.post('/launch_restore', function(req, res) {
+    controllers.launchRestore(req, res);
+  });
+
+  app.post('/restore_is_start', function(req, res) {
+    controllers.startRestore(req, res);
+  });
+
+};
