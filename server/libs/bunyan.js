4c61a8f00d24bb70e1a72d0eed4c5b0f1487bfae
vuongc <christopher.k.vuong@gmail.com>
2016-03-31 00:36:16 +0800

    #3: Dot not initialize bunyan in bunyan.js anymore and add log model into models object and create logAdapter.js

--- server/libs/bunyan.js
+++ server/libs/bunyan.js
@@ -1,24 +1,5 @@
 module.exports = function (libs, conf) {
+
   libs.bunyan = require('bunyan');
 
-  const bunyanMongodbStream = require('bunyan-mongodb-stream')({ model: Log });
-
-  libs.logger = new libs.bunyan.createLogger({
-    name: 'sam-logger',
-    stream: [
-      {
-        level: 'info',
-        stream: bunyanMongodbStream,
-      },
-      {
-        level: 'info',
-        stream: process.stdout,
-      },
-      {
-        level: 'warn',
-        path: './log/error.log',
-      },
-    ],
-    serializers: bunyan.stdSerializers,
-  });
 };
