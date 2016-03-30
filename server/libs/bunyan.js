e8f50eb752d1207468e896dbedf68af2901f4488
vuongc <christopher.k.vuong@gmail.com>
2016-03-30 23:40:58 +0800

    #HS-3: Create mongoose.js library and remove all mongoose instance in bunyan.js library

--- server/libs/bunyan.js
+++ server/libs/bunyan.js
@@ -1,16 +1,8 @@
 module.exports = function (libs, conf) {
-  libs.mongoose = require('mongoose');
   libs.bunyan = require('bunyan');
 
-  const Log = require('../models/log').iniLogModel(libs, {});
   const bunyanMongodbStream = require('bunyan-mongodb-stream')({ model: Log });
 
-  var url = 'mongodb://';
-  url += conf.host;
-  url += ':' + conf.port;
-  url += '/' + conf.database;
-
-  libs.mongoose.connect(url);
   libs.logger = new libs.bunyan.createLogger({
     name: 'sam-logger',
     stream: [
