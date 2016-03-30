04e9ddbf9e812639d35d68e1a2ead40a69fab032
vuongc <christopher.k.vuong@gmail.com>
2016-03-31 01:04:08 +0800

    #HS-3: Add logController.js and add createChild function for adapter

---
+++ server/adapters/logAdapter.js
@@ -0,0 +1,42 @@
+//
+// Adapter Log
+//
+module.exports = function initLogAdapter(models, adapters) {
+
+  //
+  // Initialize Bunyan logger and store it in libs
+  //
+  adapters.createLogger = function (libs) {
+
+    const bunyanMongodbStream = require('bunyan-mongodb-stream')({ model: Log });
+
+    libs.logger = new libs.bunyan.createLogger({
+      name: 'sam-logger',
+      stream: [
+        {
+          level: 'info',
+          stream: bunyanMongodbStream,
+        },
+        {
+          level: 'info',
+          stream: process.stdout,
+        },
+        {
+          level: 'warn',
+          path: './log/error.log',
+        },
+      ],
+      serializers: bunyan.stdSerializers,
+    });
+  };
+
+  //
+  // Create a child for logger module and return it to managers
+  // A logger child is just a logger with an header
+  // The stream is the same as the parent's
+  //
+  adapters.createChild = function (libs, header) {
+    return libs.child(header);
+  };
+
+};
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,7 @@
 module.exports = function initControllers(libs, conf, managers) {
   const controllers = {};
 
+  require('logController.js')(libs, managers, controllers);
+
   return controllers;
 };
