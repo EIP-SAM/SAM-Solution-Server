d9045f0878f37ac1df81c12b0f73438b162874e0
Etienne Leroy <leroy.etienne@gmail.com>
2016-03-31 22:54:30 +0200

    #5: crypting implemented on passport.js

--- server/libs/passport.js
+++ server/libs/passport.js
@@ -1,7 +1,34 @@
 module.exports = function initPassport(libs, conf) {
   libs.passport = require('passport');
-  libs.LocalStrategy = require('passport-local');
-
+  libs.Strategy = require('passport-local');
+  libs.User = require('../models/users');
+  libs.sha256 = require('js-sha256');
   libs.app.use(libs.passport.initialize());
   libs.app.use(libs.passport.session());
+
+  libs.passport.use(new libs.Strategy(
+    function (username, password, cb) {
+      User.findByUserName(username).then(function (user) {
+        if (!user || user.password != sha256(password)) {
+          return cb(null, false);
+        }
+        user.save();
+        return cb(null, user);
+      }).catch(function (err) {
+        return cb(err);
+      });
+    }));
+
+    libs.passport.serializeUser(function (user, cb) {
+      cb(null, user.id);
+    });
+
+    libs.passport.deserializeUser(function (id, cb) {
+      User.findById(id)
+      .then(function (user) {
+          cb(null, user);
+        }).catch(function (err) {
+          return cb(err);
+        });
+    });
 };
