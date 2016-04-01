e25463ac6671bedf229c1c2bf11b2decd08618d1
Claire Almozinos <claire@localhost.localdomain>
2016-03-31 19:49:59 -0700

    #14: create adapter for save and saveSchedule models

--- server/adapters/index.js
+++ server/adapters/index.js
@@ -2,6 +2,8 @@ module.exports = function initAdapters(libs, conf, models, workers) {
   const adapters = {};
 
   require('./restore')(models, adapters);
+  require('./save')(models, adapters);
+  require('./saveSchedule')(models, adapters);
 
   return adapters;
 };
