836cd5a15b029419d9c0f4644ae5b69f6e5188a1
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-04-01 21:53:48 +0530

    #HS-5: Update sha256 password handling + signup & login errors check

--- server/libs/index.js
+++ server/libs/index.js
@@ -4,6 +4,7 @@ module.exports = function initNodeLibraries(conf) {
   require('./express')(libs, conf);
   require('./sequelize')(libs, conf);
   require('./sequelizeSession')(libs, conf);
+  require('./sha256')(libs, conf);
   require('./passport')(libs, conf);
   require('./connectEnsureLogin')(libs, conf);
   require('./ejs')(libs, conf);
