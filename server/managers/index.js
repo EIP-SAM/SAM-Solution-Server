170452ce775de2f8f2ffd0c3b3596a8aec6568ee
Claire Almozinos <claire@localhost.localdomain>
2016-03-31 19:52:02 -0700

    #12: create save.js manage + change name function in restore.js

--- server/managers/index.js
+++ server/managers/index.js
@@ -2,6 +2,7 @@ module.exports = function initManagers(libs, conf, adapters) {
   const managers = {};
 
   require('./restore')(adapters, managers);
+  require('./save')(adapters, managers, libs);
 
   return managers;
 };
