c4587a501341600c0e92119abafd4e5aed32814c
vuongc <christopher.k.vuong@gmail.com>
2016-04-02 11:47:49 +0800

    #3: Remove bunyan initialization from adapter and move it into bunyan library file

--- server/adapters/logAdapter.js
+++ server/adapters/logAdapter.js
@@ -4,36 +4,6 @@
 module.exports = function initLogAdapter(models, adapters) {
 
   //
-  // Initialize Bunyan logger and store it in libs
-  // @TODO: Fix model dependency
-  //
-  adapters.createLogger = function (libs) {
-
-    const Log = models.log;
-
-    const bunyanMongodbStream = require('bunyan-mongodb-stream')({ model: Log });
-
-    libs.logger = new libs.bunyan.createLogger({
-      name: 'sam-logger',
-      stream: [
-        {
-          level: 'info',
-          stream: bunyanMongodbStream,
-        },
-        {
-          level: 'info',
-          stream: process.stdout,
-        },
-        {
-          level: 'warn',
-          path: './log/error.log',
-        },
-      ],
-      serializers: bunyan.stdSerializers,
-    });
-  };
-
-  //
   // Create a child for logger module and return it to managers
   // A logger child is just a logger with an header
   // The stream is the same as the parent's
