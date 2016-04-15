eff0070a32b5b093488d758fd07e55087e00ff63
clairealmozinos <claire.almozinos@epitech.eu>
2016-04-14 19:01:27 -0700

    #2: remove from express connect-flash and create file to init it + add flash.ejs + change program_save_restore_test

--- server/libs/nodeSchedule.js
+++ server/libs/nodeSchedule.js
@@ -2,19 +2,19 @@ module.exports = function initNodeSchedule(libs) {
   libs.cron = require('node-schedule');
   libs.cron.listCron = {};
 
-  libs.cron.createCron = function(cron) {
-    return libs.cron.scheduleJob(cron, function() {
+  libs.cron.createCron = function (cron) {
+    return libs.cron.scheduleJob(cron, function () {
       console.log('Cron created');
     });
   };
 
-  libs.cron.createSaveSchedule = function(date) {
-    return libs.cron.scheduleJob(date, function() {
+  libs.cron.createSaveSchedule = function (date) {
+    return libs.cron.scheduleJob(date, function () {
       console.log('Save scheduled');
     });
   };
 
-  libs.cron.removeSaveSchedule = function(key) {
+  libs.cron.removeSaveSchedule = function (key) {
     libs.cron.listCron[key].cancel();
     delete libs.cron.listCron[key];
   };
