79f24ebe2e42d70e10c4e6c37ef768364210fabf
clairealmozinos <claire.almozinos@epitech.eu>
2016-04-21 10:50:51 -0700

    #HS-10: add flash msg + add redirect

--- server/routes/restore.js
+++ server/routes/restore.js
@@ -4,16 +4,35 @@
 module.exports = function initRestoreRoutes(app, ensureLoggedIn,
   ensureLoggedOut, controllers) {
 
-  app.get('/restore', function(req, res) {
+  app.get('/restore', function (req, res) {
     res.render('program_save_restore_test.ejs');
   });
 
-  app.post('/launch_restore', function(req, res) {
-    controllers.launchRestore(req, res);
+  app.post('/create_restore', function (req, res) {
+    controllers.createRestore(req, res);
+    req.flash('msg', 'Your restoration has been created');
+    res.redirect('/restore');
   });
 
-  app.post('/restore_is_start', function(req, res) {
+  app.post('/restore_start', function (req, res) {
     controllers.startRestore(req, res);
+    req.flash('msg', 'Your restoration has started');
+    res.redirect('/restore');
   });
 
+  app.post('/restore_finish', function (req, res) {
+    controllers.restoreFinish(req, res);
+    res.redirect('/restore');
+  });
+
+  app.post('/restore_success', function (req, res) {
+    controllers.restoreSuccess(req, res);
+    req.flash('msg', 'Your restoration has succeeded');
+    res.redirect('/restore');
+  });
+
+  app.post('/restore_fail', function (req, res) {
+    req.flash('msg', 'Your restoration has failed');
+    res.redirect('/restore');
+  });
 };
