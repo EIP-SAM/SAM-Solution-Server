6d585a0924b731a5f8fa173693d643e6ff61d315
clairealmozinos <claire.almozinos@epitech.eu>
2016-04-21 10:46:38 -0700

    #HS-12: add return + add request data from template

--- server/managers/restore.js
+++ server/managers/restore.js
@@ -3,19 +3,40 @@
 //
 module.exports = function initRestoreManagers(adapters, managers) {
 
-  managers.launchRestore = function (userId, saveId) {
-    adapters.createRestore(userId, saveId);
+  //
+  // Get data from resquest
+  // Call adapter
+  //
+  managers.createRestore = function (req, res) {
+    const userId = req.body.userId;
+    const saveId = req.body.saveId;
+    return adapters.createRestore(userId, saveId);
   };
 
-  managers.startRestore = function (restoreId) {
-    adapters.restoreIsStart(restoreId);
+  //
+  // Get data from resquest
+  // Call adapter
+  //
+  managers.startRestore = function (req, res) {
+    const restoreId = req.body.restoreId;
+    return adapters.restoreIsStart(restoreId);
   };
 
-  managers.restoreFinish = function (restoreId) {
-    adapters.restoreIsFinish(restoreId);
+  //
+  // Get data from resquest
+  // Call adapter
+  //
+  managers.restoreFinish = function (req, res) {
+    const restoreId = req.body.restoreId;
+    return adapters.restoreIsFinish(restoreId);
   };
 
-  managers.restoreSuccess = function (restoreId) {
-    adapters.restoreIsSuccess(restoreId);
+  //
+  // Get data from resquest
+  // Call adapter
+  //
+  managers.restoreSuccess = function (req, res) {
+    const restoreId = req.body.restoreId;
+    return adapters.restoreIsSuccess(restoreId);
   };
 };
