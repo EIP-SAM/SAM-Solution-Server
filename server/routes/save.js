9e68b5df7a5403146458cc8c71b65cf7ed2fba54
clairealmozinos <claire.almozinos@epitech.eu>
2016-04-21 20:11:41 -0700

    #10: add redirect + flash msg

--- server/routes/save.js
+++ server/routes/save.js
@@ -12,13 +12,35 @@ module.exports = function initSaveRoutes(app) {
 
   app.post('/create_save', function (req, res) {
     saveController.createSave(req, res);
+    req.flash('msg', 'Your save has been created');
+    res.redirect('/save');
   });
 
-  app.post('/save_is_start', function (req, res) {
+  app.post('/save_start', function (req, res) {
     saveController.startSave(req, res);
+    req.flash('msg', 'Your save has started');
+    res.redirect('/save');
+  });
+
+  app.post('/save_finish', function (req, res) {
+    restoreController.saveFinish(req, res);
+    res.redirect('/save');
   });
 
   app.post('/remove_save', function (req, res) {
     saveController.removeSave(req, res);
+    req.flash('msg', 'Your auto/program save has been removed');
+    res.redirect('/save');
+  });
+
+  app.post('/save_success', function (req, res) {
+    saveController.saveSuccess(req, res);
+    req.flash('msg', 'Your save has succeeded');
+    res.redirect('/save');
+  });
+
+  app.post('/save_fail', function (req, res) {
+    req.flash('msg', 'Your save has failed');
+    res.redirect('/save');
   });
 };
