745ee029c733da6321a86d8db7a1b05211b82d08
vuongc <christopher.k.vuong@gmail.com>
2016-04-25 17:44:20 +0800

    #3: Add logAdapterTest.js file

---
+++ server/spec/logAdapterTest.js
@@ -0,0 +1,19 @@
+//
+// Test on log's adapters methods
+//
+
+//
+// Require all the necessary modules
+//
+const logAdapter = require('../adapters/log');
+
+// const mongoose = require('../libs/mongoose');
+
+describe('log managers methods', function () {
+  it('should return all logs from database', function () {
+    var a = 1;
+    var b = 1;
+
+    expect(a).toEqual(b);
+  });
+});
