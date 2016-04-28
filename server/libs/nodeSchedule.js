824f008557a341cb576fca97cd352616ed20327e
clairealmozinos <claire.almozinos@epitech.eu>
2016-04-28 16:54:33 -0700

    #2: correct merge

--- server/libs/nodeSchedule.js
+++ server/libs/nodeSchedule.js
@@ -2,21 +2,21 @@
 var cron = require('node-schedule');
 cron.listCron = {};
 
-cron.createCron = function (cron) {
-  return libs.cron.scheduleJob(cron, function () {
+cron.createCron = function (cronNotation) {
+  return cron.scheduleJob(cronNotation, function () {
     console.log('Cron created');
   });
 };
 
 cron.createSaveSchedule = function (date) {
-  return libs.cron.scheduleJob(date, function () {
+  return cron.scheduleJob(date, function () {
     console.log('Save scheduled');
   });
 };
 
 cron.removeSaveSchedule = function (key) {
-  libs.cron.listCron[key].cancel();
-  delete libs.cron.listCron[key];
+  cron.listCron[key].cancel();
+  delete cron.listCron[key];
 };
 
 module.exports = cron;
