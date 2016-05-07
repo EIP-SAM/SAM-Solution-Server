a1b2f8bd43972b59cb81275acaed84bc79ea3deb
clairealmozinos <claire.almozinos@epitech.eu>
2016-05-06 18:37:27 -0700

    #HS-12: clean nodeSchedule and create cron manager + add module cron-parser

--- server/libs/nodeSchedule.js
+++ server/libs/nodeSchedule.js
@@ -1,22 +1,11 @@
-
+//
+// Init node-schedule
+//
 var cron = require('node-schedule');
+
+//
+// List of all cron
+//
 cron.listCron = {};
-
-cron.createCron = function (cronNotation) {
-  return cron.scheduleJob(cronNotation, function () {
-    console.log('Cron created');
-  });
-};
-
-cron.createSaveScheduled = function (date) {
-  return cron.scheduleJob(date, function () {
-    console.log('Save scheduled');
-  });
-};
-
-cron.removeSaveSchedule = function (key) {
-  cron.listCron[key].cancel();
-  delete cron.listCron[key];
-};
 
 module.exports = cron;
