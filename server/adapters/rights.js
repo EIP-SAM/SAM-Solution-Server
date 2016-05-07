ac0f1ab7022516b8d2f6ab9ce221b8fe3c435689
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-05-07 07:47:54 +0530

    #5: Delete Rights model and adapter

--- server/adapters/rights.js
+++
@@ -1,9 +0,0 @@
-const RightsModel = require('../models/rights');
-
-module.exports.findById = function (id) {
-  return RightsModel.findOne({ where: { id: id } });
-};
-
-module.exports.createRight = function (data) {
-  return RightsModel.create({ data: data });
-};
