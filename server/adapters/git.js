0db32061b92c3cf004fa9a6b13456476494c3f1f
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-05-08 12:56:43 -0700

    #2: correct bug + add getLastCommitInfo()

--- server/adapters/git.js
+++ server/adapters/git.js
@@ -7,5 +7,12 @@ var gitWorker = require('../workers/git');
 // Create new repo for a user
 //
 module.exports.initNewGitRepo = function (userId, userName) {
-  return gitWorker.initNewGitRepo(userID, userName);
+  return gitWorker.initNewGitRepo(userId, userName);
+};
+
+//
+// Get last commit for a given repo
+//
+module.exports.getLastCommitInfo = function (userId, userName) {
+  return gitWorker.getLastCommitInfo(userId, userName);
 };
