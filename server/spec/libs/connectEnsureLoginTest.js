e26066c6431e67e2d372efd01339844343648004
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-05-18 14:51:07 -0700

    #HS-0: add connecte-ensure-login test

---
+++ server/spec/libs/connectEnsureLoginTest.js
@@ -0,0 +1,12 @@
+//
+// Unit test for bunyan lib
+//
+var libEnsureLogin = require('../../libs/connectEnsureLogin');
+var EnsureLogin = require('connect-ensure-login');
+
+describe('connectEnsureLogin', function () {
+
+  it('should return an EnsureLogin object', function () {
+    expect(libEnsureLogin).toBe(EnsureLogin);
+  });
+});
