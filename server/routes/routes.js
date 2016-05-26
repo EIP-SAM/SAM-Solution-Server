4ef63d0e280faff39fc1d201faac504e2ec8cb57
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-05-26 15:47:00 -0700

    #15: update route to reach electron

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,11 +5,5 @@ module.exports = function initBaseRoutes(app, conf, passport) {
   //// GET requests
 
   // Public static folder access
-  app.use(express.static(conf.rootFolder + '/app'));
-
-  // Main page
-  app.get('/', function (req, res) {
-    res.sendFile(conf.rootFolder + '/app/index.html');
-    //res.redirect('/users_and_rights/login_signup');
-  });
+  app.use(express.static(conf.rootFolder + '/webapp/build'));
 };
