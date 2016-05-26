02d24eb9e008458c8d6003e6dc1279d0e7434403
clairealmozinos <claire.almozinos@epitech.eu>
2016-05-26 14:30:50 -0700

    #15: start creating base of react + remove public forder + remove ejs

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,10 +5,11 @@ module.exports = function initBaseRoutes(app, conf, passport) {
   //// GET requests
 
   // Public static folder access
-  app.use(express.static(conf.rootFolder + '/public'));
+  app.use(express.static(conf.rootFolder + '/app'));
 
   // Main page
   app.get('/', function (req, res) {
-    res.redirect('/users_and_rights/login_signup');
+    res.sendFile(conf.rootFolder + '/app/index.html');
+    //res.redirect('/users_and_rights/login_signup');
   });
 };
