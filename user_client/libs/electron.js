438dc8f4499428db7f6f48eb758d706e78eb0b8f
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-06-03 16:05:56 +0200

    #0: rm client dir

--- user_client/libs/electron.js
+++
@@ -1,6 +0,0 @@
-module.exports = function initElectron(libs, conf) {
-  libs.electron = require('electron');
-
-  libs.app = libs.electron.app;
-  libs.BrowserWindow = libs.electron.BrowserWindow;
-};
