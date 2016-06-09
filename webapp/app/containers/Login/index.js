adf6713275c797f7cbee951b4bf0fc89880794e7
leroy-p <leroy.etienne@gmail.com>
2016-06-09 12:29:07 +0200

    #28: login page created

--- webapp/app/containers/Login/index.js
+++ webapp/app/containers/Login/index.js
@@ -3,15 +3,15 @@
 //
 
 import React from 'react';
-import { FormGroup, FormControl, Button, ControlLabel, PageHeader } from 'react-bootstrap';
+import { FormGroup, FormControl, Button, ControlLabel, PageHeader, Image } from 'react-bootstrap';
 import Logo from '../App/logo_sam_solution.png';
 import styles from 'components/Login/styles.css';
 
 export default class LoginContainer extends React.Component {
   render() {
     return (
-        <div className={styles.login}>
-          <PageHeader>Log in</PageHeader>
+        <div container className={styles.login}>
+        <Image src={Logo} responsive className={styles.logo} />
           <form>
             <FormGroup controlId="formBasicText">
               <ControlLabel>Username</ControlLabel>
@@ -21,6 +21,7 @@ export default class LoginContainer extends React.Component {
               <Button type="submit">Submit</Button>
             </FormGroup>
           </form>
+          <a href="">Forget password</a>
         </div>
     );
   }
