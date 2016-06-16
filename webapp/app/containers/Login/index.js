457d358e60da5b867eedb8c0e5c7fd659044f10c
leroy-p <leroy.etienne@gmail.com>
2016-06-16 17:32:51 +0200

    npm build start errors corrected, ChangePassword edited to EditProfile

--- webapp/app/containers/Login/index.js
+++ webapp/app/containers/Login/index.js
@@ -3,49 +3,50 @@
 //
 
 import React from 'react';
-import { FormGroup, FormControl, Button, ControlLabel, PageHeader, Image } from 'react-bootstrap';
+import { FormGroup, FormControl, Button, ControlLabel, Image } from 'react-bootstrap';
 import Logo from '../App/logo_sam_solution.png';
 import styles from 'components/Login/styles.css';
 
 export default class LoginContainer extends React.Component {
   getInitialState() {
-    return {username: '', password: ''};
+    return { username: '', password: '' };
   }
 
   handleUsernameChange(e) {
-    this.setState({username: e.target.value});
+    this.setState({ username: e.target.value });
   }
 
   handlePasswordChange(e) {
-    this.setState({password: e.target.value});
+    this.setState({ password: e.target.value });
   }
 
   handleSubmit(e) {
-    e.preventDefault;
-    var username = this.state.username;
-    var password = this.state.password;
+    const username = this.state.username;
+    const password = this.state.password;
+    e.preventDefault();
     if (!username || !password) {
       return;
     }
     // call function to log in
-    this.setState({username: '', password: ''});
+    // /users_and_rights/login'
+    this.setState({ username: '', password: '' });
   }
 
   render() {
     return (
-        <div container className={styles.login}>
+      <div container className={styles.login}>
         <Image src={Logo} responsive className={styles.logo} />
-          <form>
-            <FormGroup controlId="formBasicText">
-              <ControlLabel>Username</ControlLabel>
-              <FormControl type="text" placeholder="Enter text"/>
-              <ControlLabel>Password</ControlLabel>
-              <FormControl type="password" placeholder="Enter text"/>
-              <Button type="submit">Submit</Button>
-            </FormGroup>
-          </form>
-          <a href="">Forget password</a>
-        </div>
+        <form>
+          <FormGroup controlId="formBasicText">
+            <ControlLabel>Username</ControlLabel>
+            <FormControl type="text" placeholder="Enter text" />
+            <ControlLabel>Password</ControlLabel>
+            <FormControl type="password" placeholder="Enter text" />
+            <Button type="submit">Submit</Button>
+          </FormGroup>
+        </form>
+        <a href="">Forget password</a>
+      </div>
     );
   }
 }
