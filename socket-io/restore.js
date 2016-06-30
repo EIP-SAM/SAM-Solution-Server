42d0444f6255d63a24baff3f98db575e4f6801a7
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-06-30 17:41:17 +0200

    #31: upgrade callback use for socketIO

--- socket-io/restore.js
+++ socket-io/restore.js
@@ -1,14 +1,11 @@
 module.exports.exec = function exec(username, commit, cb) {
   let socketArray = require('./index').socketArray;
   let socket = socketArray[username];
-  console.log(socketArray);
 
   if (typeof socket !== 'undefined') {
     socket.emit('server_restore_Exec', {commit: commit});
     if (typeof cb !== 'undefined') {
-      socket.on('daemon_restore_Exec', function(msg) {
-        cb(msg);
-      })
+      socket.on('daemon_restore_Exec', cb);
     }
 
     return 1;
