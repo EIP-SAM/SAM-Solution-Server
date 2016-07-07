c201e0e86e918b605f50faa7d09d580b126aaf1b
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-07-07 15:11:05 +0200

    #29: Working commit, clean and finished, there is sample data, documentation is the next step

--- routes/statistic.js
+++ routes/statistic.js
@@ -8,25 +8,13 @@ module.exports = function initStatisticRoutes(app, conf) {
   //
   //// GET requests
 
-  app.get('/statistic_home', function (req, res) {
-    var data = statController.getAllStatistics();
-    res.render('statistic_main_view.ejs', { data: data });
-  });
-
-  app.get('/statistic_data', function (req, res) {
-    var data = statController.getStatisticData();
-    res.json(data);
-  });
-
   app.get('/statistic_filters', function (req, res) {
     var data = statController.getStatisticFilters();
     res.json(data);
   });
 
   app.get('/statistic_select_graph', function (req, res) {
-    console.log(req.get('type'));
     var data = statController.getStatisticDataByType(req.get('type'));
-    console.log(data);
     res.json(data);
   });
 };
