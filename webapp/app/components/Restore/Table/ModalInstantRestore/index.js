7724e1d7a4b20e10184406874b2e78a1259e9054
phong-steph <phong.steph@gmail.com>
2016-07-21 05:14:17 +0200

    #104: Instant restore added

---
+++ webapp/app/components/Restore/Table/ModalInstantRestore/index.js
@@ -0,0 +1,55 @@
+//
+// Modal to create an instant save in save history page
+//
+
+import React from 'react';
+import { Modal, ButtonToolbar, Glyphicon } from 'react-bootstrap';
+import { LinkContainerButton } from 'components/Button';
+import styles from 'components/RestoreHistory/Table/ModalInstantRestore/styles.css';
+
+/* eslint-disable react/prefer-stateless-function */
+export class RestoreInstantRestoreModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLaunchClick = this.handleLaunchClick.bind(this);
+    this.handleCancelClick = this.handleCancelClick.bind(this);
+  }
+
+  handleLaunchClick() {
+    this.props.createRestoresRequest(this.props.creationState, false);
+    this.props.hideInstantRestoreModal();
+  }
+
+  handleCancelClick() {
+    this.props.resetStateCreationRestore();
+    this.props.hideInstantRestoreModal();
+  }
+
+  render() {
+    return (
+      <Modal show={this.props.historyState.showModal} onHide={this.handleCancelClick}>
+        <Modal.Header closeButton>
+          <Modal.Title><Glyphicon glyph="floppy-open" className={styles.icon} />Instant restore</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={styles.p}>
+          <p>You are about to launch restore.</p>
+          <p className={styles.bold}>Are you sure that you want to launch it?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <ButtonToolbar>
+            <LinkContainerButton buttonType="info" buttonText="Launch" onClick={this.handleLaunchClick} />
+            <LinkContainerButton buttonType="default" buttonText="Cancel" onClick={this.handleCancelClick} />
+          </ButtonToolbar>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+RestoreInstantRestoreModal.propTypes = {
+  creationState: React.PropTypes.object,
+  historyState: React.PropTypes.object.isRequired,
+  hideInstantRestoreModal: React.PropTypes.func.isRequired,
+  createRestoresRequest: React.PropTypes.func,
+  resetStateCreationRestore: React.PropTypes.func,
+};
