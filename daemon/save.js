1c499bfacfa9a5532c68c7f6fdc81207afeb2810
BernardJeremy <jeremy@bernard.sh>
2016-10-03 18:52:49 +0200

    #291: remove useless commented code + add usefull log in daemon module

--- daemon/save.js
+++ daemon/save.js
@@ -1,8 +1,11 @@
+const logger = require('../libs/bunyan').setModuleName('Daemon');
+
 module.exports.exec = function exec(username, files, cb) {
   let socketArray = require('./index').socketArray;
   let socket = socketArray[username];
 
   if (typeof socket !== 'undefined') {
+    logger.info('Send save exec command for', username);
     socket.emit('server_save_Exec', {files: files});
     if (typeof cb !== 'undefined') {
       socket.on('daemon_save_Exec', cb);
