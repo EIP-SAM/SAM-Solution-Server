293e581ca6dbfd353fff0493c2626b8d6b25d157
leroy-p <leroy.etienne@gmail.com>
2016-10-05 21:30:41 +0200

    #314: rework login page updated - not founctional

--- webapp/app/containers/Login/Form/reducers.js
+++ webapp/app/containers/Login/Form/reducers.js
@@ -1,5 +1,5 @@
 //
-// Login reducer
+// Login form reducer
 //
 // To add a new action:
 //
@@ -7,31 +7,3 @@
 //  case YOUR_ACTION_CONSTANT:
 //    return state.set('yourStateVariable', true);
 //
-
-import {
-  LOGIN,
-  SAVE_DATA,
-  SET_USER_INFO,
-} from './constants';
-
-const initialState = { username: 'Enter your username', password: 'Enter your password' };
-
-function loginReducer(state = initialState, action) {
-  switch (action.type) {
-    case LOGIN:
-      return Object.assign({}, state, {});
-    case SAVE_DATA:
-      return Object.assign({}, state, {
-        username: action.username,
-        password: action.password,
-      });
-    case SET_USER_INFO:
-      return Object.assign({}, state, {
-        userInfo: action.userInfo,
-      });
-    default:
-      return state;
-  }
-}
-
-export default loginReducer;
