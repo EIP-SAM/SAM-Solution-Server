ccde14b506adcf30cccfc1ab8c1cb8ad9ec91573
leroy-p <leroy.etienne@gmail.com>
2016-10-06 17:01:07 +0200

    #314: rework login page updated, actions.js & reducer.js in containers/Login/Form

--- webapp/app/containers/Login/Form/reducers.js
+++ webapp/app/containers/Login/Form/reducers.js
@@ -7,3 +7,32 @@
 //  case YOUR_ACTION_CONSTANT:
 //    return state.set('yourStateVariable', true);
 //
+
+import { combineReducers } from 'redux-immutable';
+import LoginFormUsernameReducer from './Username/reducer';
+import LoginFormPasswordReducer from './Password/reducer';
+
+import {
+  LOGIN,
+} from './constants';
+
+const initialState = {};
+
+function EditUserFormReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN:
+      return Object.assign({}, state, {
+        user: action.user,
+      });
+
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  LoginFormReducer,
+  LoginFormUsernameReducer,
+  LoginFormPasswordReducer,
+
+});
