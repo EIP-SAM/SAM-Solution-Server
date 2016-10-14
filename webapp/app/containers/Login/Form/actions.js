dbe6af28a5a309b5029e23f92c7c4253c35791a5
leroy-p <leroy.etienne@gmail.com>
2016-10-14 14:33:11 +0200

    #314: login functionnal

--- webapp/app/containers/Login/Form/actions.js
+++ webapp/app/containers/Login/Form/actions.js
@@ -14,19 +14,11 @@ import request from 'utils/request';
 import { browserHistory } from 'react-router';
 import { resetStateUsername } from './Username/actions';
 import { resetStatePassword } from './Password/actions';
-
+import { setUserInfo } from 'containers/App/actions';
 
 import {
   LOGIN,
-  SET_USER_INFO,
 } from './constants';
-
-export function login(user) {
-  return {
-    type: LOGIN,
-    user,
-  };
-}
 
 export function resetStateForm() {
   return function resetState(dispatch) {
@@ -35,29 +27,10 @@ export function resetStateForm() {
   };
 }
 
-export function setUserInfo(logged, user) {
+export function login(user) {
   return {
-    type: SET_USER_INFO,
-    userInfo: {
-      logged,
-      userId: user.id,
-      username: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-  };
-}
-
-export function resetUserInfo() {
-  return {
-    type: SET_USER_INFO,
-    userInfo: {
-      logged: false,
-      userId: '',
-      username: '',
-      email: '',
-      isAdmin: '',
-    },
+    type: LOGIN,
+    user,
   };
 }
 
@@ -72,33 +45,6 @@ export function loginRequest(username, password) {
           dispatch(login(res.body));
           dispatch(setUserInfo(true, res.body));
           browserHistory.push(`/edit-user/${res.body.id}`);
-        }
-      });
-  };
-}
-
-export function getUserInfo() {
-  return function startAction(dispatch) {
-    return request
-      .get('/api/logged-in/user/profile')
-      .end((err, res) => {
-        if (!err) {
-          dispatch(setUserInfo(true, res.body));
-        } else {
-          dispatch(resetUserInfo());
-        }
-      });
-  };
-}
-
-export function logoutRequest() {
-  return function startAction(dispatch) {
-    return request
-      .post('/api/logged-in/user/logout')
-      .end((err) => {
-        if (!err) {
-          dispatch(resetUserInfo());
-          browserHistory.push('/login');
         }
       });
   };
