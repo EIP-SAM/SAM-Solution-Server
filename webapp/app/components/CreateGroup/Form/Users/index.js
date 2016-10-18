d838e0e97192e8cfba9adec8b8dc9e20654d8689
clairealmozinos <claire.almozinos@epitech.eu>
2016-10-18 19:33:07 +0200

    #352: fix css issue on buttons

--- webapp/app/components/CreateGroup/Form/Users/index.js
+++ webapp/app/components/CreateGroup/Form/Users/index.js
@@ -7,13 +7,14 @@ import { Col } from 'react-bootstrap';
 import AllUsers from 'containers/CreateGroup/Form/Users/AllUsers';
 import SelectedUsers from 'containers/CreateGroup/Form/Users/SelectedUsers';
 import Buttons from 'containers/CreateGroup/Form/Users/Buttons';
+import styles from 'components/CreateGroup/styles.css';
 
 /* eslint-disable react/prefer-stateless-function */
 export class CreateGroupFormUsers extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.users}>
         <Col lg={5} md={5} sm={5} xs={5}>
           <AllUsers />
         </Col>
