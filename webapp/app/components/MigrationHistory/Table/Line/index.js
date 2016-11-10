5aa303e335ec5a9905d29b60aa86b4da7dd6fed0
michau-k <alex.jf.michaux@gmail.com>
2016-11-10 14:14:18 +0100

    #395: format date and add action popover

--- webapp/app/components/MigrationHistory/Table/Line/index.js
+++ webapp/app/components/MigrationHistory/Table/Line/index.js
@@ -3,9 +3,12 @@
 //
 
 import React from 'react';
+import moment from 'moment';
 import Tr from 'components/Tr';
 import Td from 'components/Td';
 import { Label } from 'react-bootstrap';
+import { ButtonPopover } from 'components/ButtonPopover';
+import styles from './styles.css';
 
 const statusLabel = {
   done: 'success',
@@ -23,17 +26,63 @@ export default class Line extends React.Component {
     );
   }
 
+  getFormatedDate() {
+    return moment(this.props.values.date).format('YYYY MMMM Do HH:mm:ss');
+  }
+
+  getActions() {
+    if (this.props.values.status === 'planned') {
+      return (
+        <span>
+          <ButtonPopover
+            buttonStyle={styles.trash}
+            id="delete_migration"
+            placement="bottom"
+            trigger={['focus', 'hover']}
+            popoverContent="Delete Migration"
+            buttonType="link"
+            icon="trash"
+            link=""
+          />
+          <ButtonPopover
+            id="edit_migration"
+            trigger={['focus', 'hover']}
+            placement="bottom"
+            popoverContent="Edit Migration"
+            buttonType="link"
+            icon="pencil"
+            link=""
+          />
+        </span>
+      );
+    } else if (this.props.values.status === 'done') {
+      return (
+        <ButtonPopover
+          buttonStyle={styles.trash}
+          id="delete_migration"
+          trigger={['focus', 'hover']}
+          placement="bottom"
+          popoverContent="Delete Migration"
+          buttonType="link"
+          icon="trash"
+          link=""
+        />
+      );
+    }
+    return (<span></span>);
+  }
+
   render() {
     return (
       <Tr
         items={[
           { isLink: false, value: this.props.index },
           { isLink: false, value: this.getStatusLabel() },
-          { isLink: false, value: this.props.values.date },
+          { isLink: false, value: this.getFormatedDate() },
           { isLink: false, value: this.props.values.userName },
           { isLink: false, value: this.props.values.image },
           { isLink: false, value: this.props.values.comment },
-          { isLink: false, value: 'EDIT (NYI)' },
+          { isLink: false, value: this.getActions() },
         ]}
         component={Td}
       />
