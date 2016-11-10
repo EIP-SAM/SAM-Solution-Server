79fdd59c7381378695893065fbe151ea4d289a79
clairealmozinos <claire.almozinos@epitech.eu>
2016-11-10 12:16:20 +0100

    #359: create page + add link in users page + link to edit user

---
+++ webapp/app/containers/DashboardByUser/index.js
@@ -0,0 +1,21 @@
+//
+// Container dashboard by user page
+//
+
+import { connect } from 'react-redux';
+import { DashboardByUser } from 'components/DashboardByUser';
+
+function mapStateToProps() {
+  return {
+  };
+}
+
+function mapDispatchToProps() {
+  return {
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DashboardByUser);
