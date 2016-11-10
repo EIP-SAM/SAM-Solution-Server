c60487b37c797076ffdf50f5bc6962a0147cfbae
clairealmozinos <claire.almozinos@epitech.eu>
2016-11-10 16:55:57 +0100

    #359: fix error

--- webapp/app/components/DashboardByUser/index.js
+++ webapp/app/components/DashboardByUser/index.js
@@ -4,9 +4,6 @@
 
 import React from 'react';
 import DashboardByUserHeaderPage from 'containers/DashboardByUser/HeaderPage';
-import { PageHeader } from 'react-bootstrap';
-import { ButtonPopover } from 'components/ButtonPopover';
-import styles from './styles.css';
 
 /* eslint-disable react/prefer-stateless-function */
 export class DashboardByUser extends React.Component {
