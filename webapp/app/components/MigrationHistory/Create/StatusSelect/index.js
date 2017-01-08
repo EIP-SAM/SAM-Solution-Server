a6e9488e7a886a676ee8fd1b6ff7330e0abc59ce
Christopher Vuong <christopher.k.vuong@gmail.com>
2017-01-08 01:19:34 +0100

    #526: First implementation of custom Timerpicker in migration page

--- webapp/app/components/MigrationHistory/Create/StatusSelect/index.js
+++ webapp/app/components/MigrationHistory/Create/StatusSelect/index.js
@@ -3,6 +3,7 @@
 //
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   FormGroup,
   FormControl,
@@ -11,6 +12,7 @@ import {
 } from 'react-bootstrap';
 import RadioGroup from 'components/RadioGroup';
 import DatePicker from 'components/DatePicker';
+import Timepicker from 'components/Timepicker';
 import moment from 'moment';
 import styles from './styles.css';
 
@@ -33,8 +35,18 @@ export default class StatusSelect extends React.Component {
     }
   }
 
+  handleRemoveTime(e) {
+    this.props.setCreateTime('');
+  }
+
   render() {
     const time = (this.props.time !== undefined) ? this.props.time: moment().format('HH:mm');
+    const timepickerProps = {
+      time: this.props.time,
+      label: 'Time',
+      updateTimeCallback: this.handleTimepickerOnChange.bind(this),
+      handleRemove: this.handleRemoveTime.bind(this)
+    }
 
     return (
       <FormGroup controlId="time" className="clearfix">
@@ -43,11 +55,22 @@ export default class StatusSelect extends React.Component {
           <DatePicker value={this.props.date} onChange={this.props.setCreateDate} />
         </Col>
         <Col sm={6}>
-          <ControlLabel>Time</ControlLabel>
-          <FormControl type="time" value={time} onChange={this.handleTimeChange.bind(this)} />
+          <Timepicker {...timepickerProps}/>
         </Col>
       </FormGroup>
     );
+  }
+
+  //
+  // Timepicker on change date handleRemove 
+  //
+  handleTimepickerOnChange(value) {
+    if (!moment(value, ['h:mm A']).isValid()) {
+      this.props.setCreateTime('');
+    } else {
+      const time = moment(value, ['h:mm A']).format('HH:mm');
+      this.props.setCreateTime(time);
+    }
   }
 }
 
