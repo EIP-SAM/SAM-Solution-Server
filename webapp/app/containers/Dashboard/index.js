f316c8a334fdaed782d6a54b03a5d534e0d80cd4
Christopher Vuong <christophervuong@Christophers-MacBook-Pro.local>
2017-01-12 13:28:38 +0100

    #529: Remove all actions to get number of client connected to deamon

--- webapp/app/containers/Dashboard/index.js
+++ webapp/app/containers/Dashboard/index.js
@@ -7,14 +7,12 @@ import Dashboard from 'components/Dashboard';
 import {
   getSavesNumbers,
   getRestoresNumbers,
-  getDeamonUsersConnected,
  } from './actions';
 
 function mapStateToProps(state) {
   return {
     saveNumbers: state.get('dashboard').saveNumbers,
     restoreNumbers: state.get('dashboard').restoreNumbers,
-    deamonUsersConnected: state.get('dashboard').deamonUsersConnected,
     currentUserName: state.get('app').userInfo.username,
   };
 }
@@ -23,7 +21,6 @@ function mapDispatchToProps(dispatch) {
   return {
     getSavesNumbers: username => dispatch(getSavesNumbers(username)),
     getRestoresNumbers: username => dispatch(getRestoresNumbers(username)),
-    getDeamonUsersConnected: () => dispatch(getDeamonUsersConnected()),
   };
 }
 
