26ad82f8c174cb2ff853cb419ae1f7c8020d5bb0
clairealmozinos <claire.almozinos@epitech.eu>
2017-01-12 17:22:33 +0100

    #359: finalisation merge

---
+++ webapp/app/components/DashboardByUser/Logs/index.js
@@ -0,0 +1,18 @@
+//
+// Component dashboard by user page
+//
+
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export class DashboardByUserLogs extends React.Component {
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+DashboardByUserLogs.propTypes = {
+};
