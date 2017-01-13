729e38c7d86563a954d99f36bc8df3f3edf6e540
clairealmozinos <claire.almozinos@epitech.eu>
2017-01-13 16:26:01 +0100

    #359: add label to logs

--- webapp/app/components/DashboardByUser/Logs/index.js
+++ webapp/app/components/DashboardByUser/Logs/index.js
@@ -4,10 +4,11 @@
 
 import React from 'react';
 import moment from 'moment';
-import { Table } from 'react-bootstrap';
+import { Table, Label } from 'react-bootstrap';
 import Tr from 'components/Tr';
 import Th from 'components/Th';
 import Td from 'components/Td';
+import levels from 'components/Logs/Results/levels.json';
 import styles from './styles.css';
 
 /* eslint-disable react/prefer-stateless-function */
@@ -38,7 +39,7 @@ export default class DashboardByUserLogs extends React.Component {
             <Tr
               key={`row-${index}`} items={[
                 { isLink: false, value: moment(log.time).format('YYYY MMMM Do HH:mm:ss') },
-                { isLink: false, value: log.level },
+                { isLink: false, value: <Label bsStyle={levels[log.level].style}>{levels[log.level].name}</Label> },
                 { isLink: false, value: log.moduleName },
                 { isLink: false, value: log.msg }]} component={Td}
             />
