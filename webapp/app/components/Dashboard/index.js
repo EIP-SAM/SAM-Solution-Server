e81de0c47d34743e0a4def75c4e2fee8c1f50dde
Christopher Vuong <christopher.k.vuong@gmail.com>
2017-01-18 22:26:15 +0100

    #589: Home menu is done

--- webapp/app/components/Dashboard/index.js
+++ webapp/app/components/Dashboard/index.js
@@ -9,46 +9,102 @@ import BlockInfo from 'containers/Dashboard/BlockInfo';
 export default class Dashboard extends React.Component {
   componentWillMount() {
     this.username = this.props.currentUserName;
-    this.props.getSavesNumbers(this.username);
-    this.props.getRestoresNumbers(this.username);
-  }
-
-  componentWillUpdate() {
-    this.props.getSavesNumbers(this.username);
-    this.props.getRestoresNumbers(this.username);
   }
 
   render() {
     const username = this.props.currentUserName;
-    const saveNumbers = this.props.saveNumbers || 0;
-    const restoreNumbers = this.props.restoreNumbers || 0;
 
     return (
       <div>
         <h1>Dashboard <small>Welcome {username}</small></h1>
         <hr />
-        <Grid bsClass="">
+        <Grid fluid bsClass="">
           <Row>
-            <Col md={6} lg={3}>
+
+            {/* Users */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="user"
+                title="Module users"
+                msg="You can manage all the users of the application: create, edit or remove them"
+                color="blue"
+                text="More details"
+                link="/users"
+              />
+            </Col>
+
+            {/* Groups */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="tags"
+                title="Module groups"
+                msg="You can also manage the users by grouping them. Consequently, you can create edit or remove a group"
+                color="green"
+                text="More details"
+                link="/groups"
+              />
+            </Col>
+
+            {/* Save */}
+            <Col md={6} lg={4}>
               <BlockInfo
                 icon="floppy-disk"
-                title="Save"
-                msg={saveNumbers.toString()}
-                color="blue"
+                title="Module save"
+                msg="You can create an instant save, scheduled a one shot save or scheduled an auto save"
+                color="orange"
                 text="More details"
                 link="/save"
               />
             </Col>
-            <Col md={6} lg={3}>
+
+            {/* Restore */}
+            <Col md={6} lg={4}>
               <BlockInfo
                 icon="repeat"
-                title="Restore"
-                msg={restoreNumbers.toString()}
-                color="green"
+                title="Module restore"
+                msg="You can restore data from a previous save"
+                color="red"
                 text="More details"
                 link="/restore"
               />
             </Col>
+
+            {/* Softwares */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="cd"
+                title="Module softwares"
+                msg="You can install, update or remove software in an user's computer"
+                color="blue"
+                text="More details"
+                link="/software"
+              />
+            </Col>
+
+            {/* Logs */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="list"
+                title="Module logs"
+                msg="You can check every action made on the application in the logs module. There are several levels of logs : info, warn, error and fatal"
+                color="green"
+                text="More details"
+                link="/logs"
+              />
+            </Col>
+
+            {/* Notifications */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="envelope"
+                title="Module notifications"
+                msg="You can send a notification to one or several users"
+                color="orange"
+                text="More details"
+                link="/notifications"
+              />
+            </Col>
+
           </Row>
         </Grid>
       </div>
@@ -58,8 +114,4 @@ export default class Dashboard extends React.Component {
 
 Dashboard.propTypes = {
   currentUserName: React.PropTypes.string,
-  saveNumbers: React.PropTypes.number,
-  restoreNumbers: React.PropTypes.number,
-  getSavesNumbers: React.PropTypes.func,
-  getRestoresNumbers: React.PropTypes.func,
 };
--- webapp/app/containers/Dashboard/index.js
+++ webapp/app/containers/Dashboard/index.js
@@ -4,24 +4,15 @@
 
 import { connect } from 'react-redux';
 import Dashboard from 'components/Dashboard';
-import {
-  getSavesNumbers,
-  getRestoresNumbers,
- } from './actions';
 
 function mapStateToProps(state) {
   return {
-    saveNumbers: state.get('dashboard').saveNumbers,
-    restoreNumbers: state.get('dashboard').restoreNumbers,
     currentUserName: state.get('app').userInfo.username,
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    getSavesNumbers: username => dispatch(getSavesNumbers(username)),
-    getRestoresNumbers: username => dispatch(getRestoresNumbers(username)),
-  };
+function mapDispatchToProps() {
+  return {};
 }
 
 export default connect(
