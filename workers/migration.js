193741300aeed5c521b500b18ff6966b1abaead7
BernardJeremy <jeremy@bernard.sh>
2017-01-18 11:20:06 +0100

    #578: add cmd exec on migration

--- workers/migration.js
+++ workers/migration.js
@@ -2,9 +2,13 @@
 // Worker Migration
 //
 
+const exec = require('child_process').exec;
+
 const logger = require('../libs/bunyan').setModuleName('Migration');
 const rebootCommand = require('../websocket/daemon/reboot');
 const getMacAdressCommand = require('../websocket/daemon/getMacAdress');
+
+const configCmd = require('../config/base.config.json').onMigrationExecCmd;
 
 function rebootDaemon(userName) {
   return new Promise((fullfill) => {
@@ -24,6 +28,28 @@ function getMacAdress(userName) {
   });
 }
 
+function execMigrationCmd(macAddress, imagePath) {
+  return new Promise((fullfill, reject) => {
+    let cmd = '';
+    if (!configCmd || configCmd === '') {
+      fullfill('Command not found in config, migration continuing');
+    } else {
+      cmd = configCmd.replace('#macaddress#', macAddress).replace('#imagepath#', imagePath);
+
+      logger.info(`Exec now DRBL cmd : ${cmd}`);
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          logger.debug(`DRBL cmd error output : ${stderr}`);
+          reject(`${error}`);
+          return;
+        }
+        logger.debug(`DRBL cmd output : ${stdout}`);
+        fullfill('DRBL command executed');
+      });
+    }
+  });
+}
+
 //
 // Exec a migration for a specific user
 //
@@ -36,8 +62,14 @@ module.exports.execMigration = (userName, imagePath) =>
         return reject(`Unable to perform migration for ${userName}, Mac Address unavailable`);
       }
       logger.info(`Address Mac for ${userName} : ${macAddress}`);
-      return rebootDaemon(userName).then(() => {
-        fullfill(`Migration started for ${userName}`);
+      return execMigrationCmd(macAddress, imagePath).then((msg) => {
+        logger.info(`Exec DRBL command went OK : ${msg}`);
+        return rebootDaemon(userName).then(() => {
+          fullfill(`Migration started for ${userName}`);
+        });
+      }).catch((err) => {
+        logger.error(`${err}`);
+        reject(err);
       });
     });
   });
