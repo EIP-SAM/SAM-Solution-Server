17208a388b6c448867c57b6e8a9c440b234e1ae6
Christopher Vuong <christopher.k.vuong@gmail.com>
2017-01-19 11:30:34 +0100

    #589: Add button on homepage

--- webapp/app/components/Homepage/index.js
+++ webapp/app/components/Homepage/index.js
@@ -28,7 +28,7 @@ export default class Homepage extends React.Component {
                 title="Module users"
                 msg="You can manage all the users of the application: create, edit or remove them"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/users"
               />
             </Col>
@@ -40,7 +40,7 @@ export default class Homepage extends React.Component {
                 title="Module groups"
                 msg="You can also manage the users by grouping them. Consequently, you can create edit or remove a group"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/groups"
               />
             </Col>
@@ -52,7 +52,7 @@ export default class Homepage extends React.Component {
                 title="Module save"
                 msg="You can create an instant save, scheduled a one shot save or scheduled an auto save"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/save"
               />
             </Col>
@@ -64,8 +64,32 @@ export default class Homepage extends React.Component {
                 title="Module restore"
                 msg="You can restore data from a previous save"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/restore"
+              />
+            </Col>
+
+            {/* Migration */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="repeat"
+                title="Module migration"
+                msg="You can migration one or several users into a new OS"
+                color="blue"
+                text="Navigate to page"
+                link="/migration/history"
+              />
+            </Col>
+
+            {/* Images */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="repeat"
+                title="Module images"
+                msg="You can manage OS images by adding or removing them"
+                color="blue"
+                text="Navigate to page"
+                link="/images"
               />
             </Col>
 
@@ -76,7 +100,7 @@ export default class Homepage extends React.Component {
                 title="Module softwares"
                 msg="You can install, update or remove software in an user's computer"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/software"
               />
             </Col>
@@ -88,8 +112,20 @@ export default class Homepage extends React.Component {
                 title="Module logs"
                 msg="You can check every action made on the application in the logs module. There are several levels of logs : info, warn, error and fatal"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/logs"
+              />
+            </Col>
+
+            {/* Statistics */}
+            <Col md={6} lg={4}>
+              <BlockInfo
+                icon="stats"
+                title="Module Statistics"
+                msg="You can find usefull statistics in the stats module. Number of save/restore over time, logs type repartition..."
+                color="blue"
+                text="Navigate to page"
+                link="/statistics"
               />
             </Col>
 
@@ -100,7 +136,7 @@ export default class Homepage extends React.Component {
                 title="Module notifications"
                 msg="You can send a notification to one or several users"
                 color="blue"
-                text="More details"
+                text="Navigate to page"
                 link="/notifications"
               />
             </Col>
