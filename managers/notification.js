55943e8c23a2277afb3176bb7ec6cac9f06777f2
BernardJeremy <jeremy@bernard.sh>
2017-01-19 17:46:34 +0100

    #499: add backend group management for notification

--- managers/notification.js
+++ managers/notification.js
@@ -1,26 +1,50 @@
 const logger = require('../libs/bunyan').setModuleName('Notification');
 
+const groupAdapter = require('../adapters/groups');
+
 const daemonNotification = require('../websocket/daemon/notification');
 
+function displayNotification(usernames, title, description, persistence) {
+  for (let i = 0; i < usernames.length; ++i) {
+    if (usernames[i].name !== undefined) {
+      usernames[i] = usernames[i].name;
+    }
+
+    logger.info(`Notification display asked for ${usernames[i]}`);
+    daemonNotification.display(usernames[i], title, description, persistence, () => {
+      logger.info(`Notification displayed for ${usernames[i]}`);
+    });
+  }
+}
+
 module.exports.displayNotificationByUsername = (req) => {
-  let username = req.body.username;
+  const username = req.body.username;
   const title = req.body.title;
   const description = req.body.description;
   const persistence = req.body.persistence;
+  const isGroup = req.body.isGroup;
 
   if (!username || !title || !description) {
     return { isSuccess: false, err: 'Missing POST parameter', obj: req.body };
   }
 
+  let usernameArray = username;
   if (!Array.isArray(username)) {
-    username = [username];
+    usernameArray = [username];
   }
 
-  for (let i = 0; i < username.length; ++i) {
-    logger.info(`Notification display asked for ${username[i]}`);
-    daemonNotification.display(username[i], title, description, persistence, () => {
-      logger.info(`Notification displayed for ${username[i]}`);
-    });
+  if (isGroup && isGroup === 'true') {
+    for (let i = 0; i < usernameArray.length; ++i) {
+      groupAdapter.findAllAssociatedWithUsers().then((groups) => {
+        for (let j = 0; j < groups.length; ++j) {
+          if (usernameArray[j] === groups[j].name) {
+            displayNotification(groups[j].users, title, description, persistence);
+          }
+        }
+      });
+    }
+  } else {
+    displayNotification(usernameArray, title, description, persistence);
   }
 
   return { isSuccess: true, err: '' };
