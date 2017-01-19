e28784094c255e2051319a5e62a3c54b8aee637b
leroy-p <leroy.etienne@gmail.com>
2017-01-19 11:47:07 +0100

    #499: error connected in select group side

--- webapp/app/containers/NotificationsGroups/Form/index.js
+++ webapp/app/containers/NotificationsGroups/Form/index.js
@@ -3,7 +3,7 @@
 //
 
 import { connect } from 'react-redux';
-import { NotificationsGroupsForm } from 'components/NotificationsGroups/Form';
+import NotificationsGroupsForm from 'components/NotificationsGroups/Form';
 
 function mapStateToProps() {
   return {
--- webapp/app/containers/NotificationsGroups/index.js
+++ webapp/app/containers/NotificationsGroups/index.js
@@ -3,13 +3,12 @@
 //
 
 import { connect } from 'react-redux';
-import { NotificationsGroups } from 'components/NotificationsGroups';
+import NotificationsGroups from 'components/NotificationsGroups';
 import { resetStateForm } from '../Notifications/Form/actions';
 import { resetAlert } from '../Notifications/actions';
 
 function mapStateToProps(state) {
   return {
-    alertMsg: state.get('notifications').get('NotificationsReducer').alertMsg,
     typeAlert: state.get('notifications').get('NotificationsReducer').typeAlert,
     displayAlert: state.get('notifications').get('NotificationsReducer').displayAlert,
   };
